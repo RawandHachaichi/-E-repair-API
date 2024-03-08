dc4de5745374cb5af32ac4f4e4f64cf3b7c492bb
rawand <rawandhachaichi19@gmail.com>
2024-03-08 19:22:02 +0100

    firt=st commit

---
+++ Repair.Database/DatabaseContext.cs
@@ -0,0 +1,12 @@
+﻿using E_repair.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Repair.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Gouvernorat> Gouvernorat { get; set; }
+    }
+}
---
+++ Repair.Database/Entities/Gouvernorat.cs
@@ -0,0 +1,15 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace E_repair.Models
+{
+    public class Gouvernorat
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Nom { get; set; }
+        public string Code { get; set; }
+        public string CreePar { get; set; }
+        public DateTime DateCreation { get; set; }
+    }
+}
