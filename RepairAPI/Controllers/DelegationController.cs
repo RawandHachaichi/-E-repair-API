47f3b299d33af6866a5ccd4614d0ac1ad8574551
rawand <rawandhachaichi19@gmail.com>
2024-03-11 23:24:34 +0100

    ..

---
+++ RepairAPI/Controllers/DelegationController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Repair.Business.Interfaces;
+using Repair.Business.Models;
+using Repair.Business.Repository;
+
+namespace RepairAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DelegationController : ControllerBase
+    {
+
+        private static IDelegationRepository _IDelegationtRepository;
+
+        public DelegationController(IDelegationRepository delegationtRepository)
+        {
+            _IDelegationtRepository = delegationtRepository;
+        }
+
+
+        [HttpGet(Name = "GetDelagations/{id}")]
+        public List<DelegationModel> GetDelegations(Guid?id)
+        {
+            return _IDelegationtRepository.GetDelegationByGouvId(id);
+
+        }
+    }
+}
