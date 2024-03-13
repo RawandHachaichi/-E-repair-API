edfe8f397e993d2bbe1000f19dc9803a72a9713b
rawand <rawandhachaichi19@gmail.com>
2024-03-13 22:33:01 +0100

    ..

--- Repair.Business/obj/Debug/net6.0/Repair.Business.AssemblyInfo.cs
+++ Repair.Business/obj/Debug/net6.0/Repair.Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repair.Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f23a58fe81cd7486cd89f04686d671f4f930c2e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27afd4bbc8f3f56aefc55398ea8387a578df5f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repair.Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repair.Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
