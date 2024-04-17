4f19a829dd4217c589a8b1f7e5ff505c2aacba3e
RawandHachaichi <159036443+RawandHachaichi@users.noreply.github.com>
2024-04-17 14:15:54 +0100

    .

--- Repair.Database/obj/Debug/net6.0/Repair.Database.AssemblyInfo.cs
+++ Repair.Database/obj/Debug/net6.0/Repair.Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repair.Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af90fa30276cca120575b4c91d1f04bb8d3a24f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d563dbac0c8c9780751577ef0ed6d4e96c5c513")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repair.Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repair.Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
